82c97a5df01093dca78e752e46aeeeaacf1a60ee
pachet <pachet@burninggarden.com>
2016-08-06 19:28:11 -0400

    Laying the groundwork for private user messages

--- test.js
+++ test.js
@@ -1,23 +1,33 @@
 var Pirc = require('./index');
 
+/*
 var server = new Pirc.Server();
 
 server.setName('irc.burninggarden.com');
 server.listen(6667);
+*/
 
 var client = new Pirc.Client();
 
 client.connectToServer({
-	address: '127.0.0.1',
+	address: 'irc.freenode.org',
 	port:    6667,
-	nick:    'chaba'
+	nick:    'chabaxbe'
 }, function handler(error) {
 	if (error) {
 		return void console.warn(error);
 	}
 
-	client.joinChannel('#test', function handler(error, channel) {
-		client.sendMessageToChannel('foobar', '#test');
+	client.joinChannel('#ganondorf', function handler(error, channel) {
+
+		channel.on('message', function handler(message) {
+			console.log('CAUGHT MESSAGE:');
+			console.log('CAUGHT MESSAGE:');
+			console.log('CAUGHT MESSAGE:');
+			console.log(message);
+		});
+
+		client.sendMessageToChannel('foobar', '#ganondorf');
 	});
 
 	client.on('disconnect', function handler() {
