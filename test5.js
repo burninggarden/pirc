9ee305a152128fdf66d11db51d93ca13c0aaf87b
pachet <pachet@burninggarden.com>
2017-03-04 10:48:35 -0500

    Update RPL_TOPICWHOTIME to use author identifier, instead of just nick

--- test5.js
+++ test5.js
@@ -1,6 +1,13 @@
 
 global.req = require('req');
 
-var Regexes = req('/constants/regexes');
+var NeedMoreParamsMessage = req('/lib/server/messages/need-more-params');
 
-console.log(Regexes.NICK.test(undefined));
+var message = new NeedMoreParamsMessage();
+
+var raw_message = ':irc.burninggarden.com 461 stonefare MODE :Not enough parameters.';
+
+message.setRawMessage(raw_message);
+message.deserialize();
+
+console.log(message.getAttemptedCommand());
