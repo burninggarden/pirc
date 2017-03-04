80441d73b5c06db3ecbd6f308e8ef40a0a69a251
pachet <pachet@burninggarden.com>
2017-03-04 11:06:39 -0500

    Add fallback logic when trying to retrieve user details for a message

--- test5.js
+++ test5.js
@@ -1,13 +1,13 @@
 
 global.req = require('req');
 
-var NeedMoreParamsMessage = req('/lib/server/messages/need-more-params');
+var ErroneousNicknameMessage = req('/lib/server/messages/erroneous-nickname');
 
-var message = new NeedMoreParamsMessage();
+var message = new ErroneousNicknameMessage();
 
-var raw_message = ':irc.burninggarden.com 461 stonefare MODE :Not enough parameters.';
+var raw_message = ':irc.burninggarden.com 432 thrullcha 䠐䜆ᇫ���䑺㜴䶓ᬻс䘎फ़ֱἰ䫯䐡₢ೃǮኟᗯᰳ㤣ឃ㋹䖀⃂㦍ଊᙳ㷬䲠㦺䵾ٹௗ⑸㔡上䣺ᓮ㕢஺䍡ᡀᐨ⥨䣬݆���῎䇠㝥❂ӓἇ䶏㴴㊺㳈ᘾġە⟘䭷જ૓ತ׳఩ڄ⢚ᄗᖌ԰ڇ䥄䛻 㹯ⷔෞ䔸؇♻὘㚓䤞᎕೵ㄜ㌦⼜䰵ᛇ⸁ᪧ೏ :Erroneous Nickname';
 
 message.setRawMessage(raw_message);
 message.deserialize();
 
-console.log(message.getAttemptedCommand());
+console.log(message.getUserDetails());
