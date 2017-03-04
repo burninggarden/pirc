c59599350fcdbf4abf6b88e7b9ed6a1a40767f0b
pachet <pachet@burninggarden.com>
2017-03-04 12:24:49 -0500

    Fix bug in message parsing error handling logic on the server

--- test3.js
+++ test3.js
@@ -1,19 +1,7 @@
 global.req = require('req');
 
-var Regexes = require('./constants/regexes');
+var MessageParser = req('/lib/server/message-parser');
 
-// var user_id = 'crilbith!~pirc@toroon0246w-lp140-02-70-27-164-136.dsl.bell.ca';
-var user_id = 'Ridgetopr!~pirc@127.0.0.1';
+var raw_message = 'MODE';
 
-// var host = 'toroon0246w-lp140-02-70-27-164-136.dsl.bell.ca';
-
-// console.log(Regexes.USER_IDENTIFIER);
-
-var match = user_id.match(Regexes.USER_IDENTIFIER);
-
-console.log(Regexes.USER_IDENTIFIER);
-
-console.log(match[1]);
-console.log(match[2]);
-console.log(match[3]);
-
+MessageParser.parse(raw_message);
