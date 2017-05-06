3e48b3250a87f84690d5680dc0180ec442270c7b
pachet <pachet@burninggarden.com>
2017-05-06 16:51:22 -0400

    First pass at breaking out server / client connections

---
+++ constants/connection-events.js
@@ -0,0 +1,17 @@
+
+module.exports = {
+
+	CONNECTION_SUCCESS:           'connection_success',
+	CONNECTION_FAILURE:           'connection_failure',
+	CONNECTION_END:               'connection_end',
+
+	INCOMING_MESSAGE:             'incoming_message',
+
+	PING:                         'ping',
+
+	REGISTER:                     'register',
+
+	UPGRADE_TO_CLIENT_CONNECTION: 'upgrade_to_client_connection',
+	UPGRADE_TO_SERVER_CONNECTION: 'upgrade_to_server_connection'
+
+};
