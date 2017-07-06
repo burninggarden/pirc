6f743e1b71d743e55de0c7321c3cb63df0cd1a26
pachet <pachet@burninggarden.com>
2017-07-06 15:17:01 -0400

    Better handle cases where a PASS message is sent post-registration

--- tests/RFC2812/3.1/3.1.1.js
+++ tests/RFC2812/3.1/3.1.1.js
@@ -24,25 +24,42 @@ var
 	Commands = req('/lib/constants/commands');
 
 function testNeedMoreParams(test) {
+	test.expect(1);
+
 	var client = test.createServerAndClient({
-		nickname:              'cloudbreaker',
-		username:              'cloudbreaker',
-		autoregister:          false,
-		log_outbound_messages: true
+		nickname:     'cloudbreaker',
+		username:     'cloudbreaker',
+		autoregister: false
 	});
 
 	client.once('connected', function handler() {
 		client.sendRawMessage('PASS');
+		client.sendRawMessage('NICK balrog');
+		client.sendRawMessage('USER balrog 0 * :balrog');
 
 		client.awaitReply(Replies.ERR_NEEDMOREPARAMS, function handler(reply) {
-			test.equals(reply.getCommand(), Commands.PASS);
+			test.equals(reply.getAttemptedCommand(), Commands.PASS);
 			test.done();
 		});
 	});
 }
 
 function testAlreadyRegistered(test) {
-	test.done();
+	test.expect(1);
+
+	var client = test.createServerAndClient({
+		nickname: 'cloudbreaker',
+		username: 'cloudbreaker'
+	});
+
+	client.once('registered', function handler() {
+		client.sendRawMessage('PASS whatever');
+
+		client.awaitReply(Replies.ERR_ALREADYREGISTRED, function handler(reply) {
+			test.ok(true, 'Received ERR_ALREADYREGISTRED reply');
+			test.done();
+		});
+	});
 }
 
 module.exports = {
